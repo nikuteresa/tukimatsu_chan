37957c8b153150f9c7a07dd161c88eab804a1057
nikuteresa <nikuteresa@gmail.com>
2025-04-23 20:21:33 +0900

    Add RSpec helper and configuration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,46 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+require 'timecop'
+require 'webmock/rspec'
+
+# ローカル環境のみでWebMockを有効にする
+WebMock.disable_net_connect!(allow_localhost: true)
+
+# spec/support ディレクトリ内のファイルを読み込む
+Dir[File.expand_path("support/**/*.rb", __dir__)].each { |f| require f }
+
+RSpec.configure do |config|
+  # 期待値の記述スタイルをexpect に設定
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # モックのフレームワークを設定
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  # 共有コンテキストの説明を繰り返さない
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  # テストを実行する前にrandom seedを表示
+  config.before(:suite) do
+    puts "\n\nUsing RSpec seed: #{config.seed}\n\n"
+  end
+
+  # 各テスト実行後、モックと変更を初期化
+  config.after(:each) do
+    # Timecopをリセット
+    Timecop.return
+  end
+  
+  # 成功、失敗に関わらずすべてのテスト結果を表示
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
+  # 実行順序をランダムにする
+  config.order = :random
+  Kernel.srand config.seed
+end
