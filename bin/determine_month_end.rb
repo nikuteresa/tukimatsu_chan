b99dc1455a40007026117f7edf3ad37f4a923d13
nikuteresa <nikuteresa@gmail.com>
2025-04-24 12:32:14 +0900

    feat: 日付入力パラメータに対応

--- bin/determine_month_end.rb
+++ bin/determine_month_end.rb
@@ -9,19 +9,33 @@ class MonthEndDeterminer
     @business_day_calculator = BusinessDayCalculator.new
   end
 
-  def run(date = Date.today)
-    is_last_business_day = @business_day_calculator.last_business_day_of_current_month?(date)
-    date = "#{date.year}年#{date.month}月#{date.day}日"
+  def run
+    # 環境変数から日付を取得（指定がなければ今日の日付を使用）
+    date_str = ENV['INPUT_DATE']
+    date = if date_str && !date_str.empty?
+      begin
+        Date.parse(date_str)
+      rescue ArgumentError
+        puts "Error: Invalid date format '#{date_str}'. Using today's date instead."
+        Date.today
+      end
+    else
+      Date.today
+    end
 
-    # 環境変数を設定
+    # 月末判定を実行
+    is_last_business_day = @business_day_calculator.last_business_day_of_current_month?(date)
+    formatted_date = "#{date.year}年#{date.month}月#{date.day}日"
+
+    # GitHub Actionsの出力を設定
     ENV['GITHUB_OUTPUT'].tap do |env_file|
       if env_file.nil?
         puts "is_last_business_day=#{is_last_business_day}"
-        puts "executed_at=#{date}"
+        puts "executed_at=#{formatted_date}"
       else
         File.open(env_file, 'a') do |file|
           file.puts "is_last_business_day=#{is_last_business_day}"
-          file.puts "executed_at=\"#{date}\""
+          file.puts "executed_at=\"#{formatted_date}\""
         end
       end
     end
